7de6656fbf85a083ee55be9057e609865751e202
beitu <1690840919@qq.com>
2022-05-18 15:10:09 +0800

    写在vetur，解决报错问题

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
-const name = 'beitu'
-</script>
-
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>{{ name }}</h1>
+    <h1>{{ age }}</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+const name = 'beitu'
+const age = 200
+</script>
 
 <style>
 </style>
