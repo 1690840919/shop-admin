99d3e8773d2ead857913c400536a0e613bec15a7
beitu <1690840919@qq.com>
2022-05-20 15:44:32 +0800

    设计了nums数组

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,6 +3,7 @@
     <h1>{{ name }}</h1>
     <h1>{{ age }}</h1>
     <h1>{{ score }}</h1>
+    <h1 v-for="i in nums" :key="i">{{ i }}</h1>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 const name = '登陆页'
 const age = 22
 const score = 3000
+const nums = [1, 2, 3]
 </script>
 
 <style>
