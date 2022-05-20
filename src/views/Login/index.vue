c21f28ae54ca356870872a5e4a4c4fb96d2173c3
beitu <1690840919@qq.com>
2022-05-20 13:22:31 +0800

    设计了score功能

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>{{ name }}</h1>
     <h1>{{ age }}</h1>
+    <h1>{{ score }}</h1>
   </div>
 </template>
 
 <script lang="ts" setup>
 const name = '登陆页'
 const age = 30
+const score = 3000
 </script>
 
 <style>
