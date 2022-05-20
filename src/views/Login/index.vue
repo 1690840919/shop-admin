40869e8fd72d607a20cc8c12bc6a081baf0593c1
beitu <1690840919@qq.com>
2022-05-20 15:56:43 +0800

    修改了登陆页面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h1>{{ name }}</h1>
+    <h1>{{ name }} {{ loginName }}</h1>
     <h1>{{ age }}</h1>
     <h1>{{ score }}</h1>
   </div>
 </template>
 
 <script lang="ts" setup>
-const name = '登陆页'
+const name = '登陆页1212'
+const loginName = 'login LOGO'
 const age = 22
 const score = 3000
 </script>
